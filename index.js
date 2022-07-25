8d49502e9521c94397347299218f9b7102fc1c68
samiksengupta <samiksengupta@hotmail.com>
2022-07-25 22:40:59 +0530

    removed dependency for body parser

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 const { HOST, PORT, ENV } = require('./config/server');
 const masterRouter = require('./routers');
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(masterRouter);
 
 app.listen(PORT, () => {
